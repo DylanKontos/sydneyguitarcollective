7ac8f9d70244dca5e17fcaf99f3f6bbca3b43637
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-05-14 11:56:09 +1000

    Update serverless_function.js

--- api/serverless_function.js
+++ api/serverless_function.js
@@ -1,5 +1,14 @@
-module.exports = (req, res) => {
-    res.json({
-        hola: 'mundo'
-    })
-}
+const { Vercel } = require('@vercel/client');
+const vercel = new Vercel({ token: 'i3gzC08QCPX5SRaxWGLoRlTQ' });
+
+module.exports = async (req, res) => {
+  try {
+    const { id: projectId } = await vercel.getProject('prj_fHOeMx8mgXC5p0smFwsFw65DmfeC');
+    const { envs } = await vercel.getEnvironmentVariables(projectId);
+    const variables = envs.reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {});
+    res.status(200).json(variables);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+};
