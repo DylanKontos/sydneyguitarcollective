9c0a322f1124e05617326e960845fa92e0547fab
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-05-14 11:58:02 +1000

    Update serverless_function.js

--- api/serverless_function.js
+++ api/serverless_function.js
@@ -3,8 +3,7 @@ const vercel = new Vercel({ token: 'i3gzC08QCPX5SRaxWGLoRlTQ' });
 
 module.exports = async (req, res) => {
   try {
-    const { id: projectId } = await vercel.getProject('prj_fHOeMx8mgXC5p0smFwsFw65DmfeC');
-    const { envs } = await vercel.getEnvironmentVariables(projectId);
+    const { envs } = await vercel.getEnvironmentVariables('prj_fHOeMx8mgXC5p0smFwsFw65DmfeC');
     const variables = envs.reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {});
     res.status(200).json(variables);
   } catch (error) {
