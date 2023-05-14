451ab8bde2650b2b2382a1bffb21a3edc58ba7be
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-05-14 13:35:01 +1000

    Update serverless_function.js

--- api/serverless_function.js
+++ api/serverless_function.js
@@ -5,19 +5,24 @@ async function refreshAccessToken() {
   // Use the Vercel environment variables for the client ID, client secret, and refresh token
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  const refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
 
-  // Use the googleapis library to generate a new access token
-  const authClient = new OAuth2(clientId, clientSecret);
-  authClient.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
-  
-  try {
-    const { access_token } = await authClient.getAccessToken();
-    console.log('Access Token:', access_token);
-    return { accessToken: access_token }
-  } catch (error) {
-    console.error('Error retrieving access token:', error);
-    return { failed: error };
-  }
+  // Set up the request parameters
+  const url = 'https://oauth2.googleapis.com/token';
+  const body = `grant_type=refresh_token&client_id=${clientId}&client_secret=${clientSecret}&refresh_token=${refreshToken}`;
+
+  // Make the request using fetch
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body,
+  });
+
+  // Extract the JSON response body
+  const json = await response.json();
+
+  // Return the new access token
+  return json.access_token;
 }
 
 module.exports = async (req, res) => {
