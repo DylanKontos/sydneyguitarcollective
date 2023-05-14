e0a79972ee285030dd84443d69afe5e87c1783c5
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-05-14 12:13:30 +1000

    Update serverless_function.js

--- api/serverless_function.js
+++ api/serverless_function.js
@@ -1,9 +1,10 @@
 const { Vercel } = require('@vercel/client');
-const vercel = new Vercel({ token: 'i3gzC08QCPX5SRaxWGLoRlTQ' });
+const vercel = new Vercel({ token: process.env.VERCEL_TOKEN });
 
 module.exports = async (req, res) => {
   try {
-    const { envs } = await vercel.getEnvironmentVariables('prj_fHOeMx8mgXC5p0smFwsFw65DmfeC');
+    const { id: projectId } = await vercel.getProject(process.env.VERCEL_PROJECT_ID);
+    const { envs } = await vercel.getEnvironmentVariables(projectId);
     const variables = envs.reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {});
     res.status(200).json(variables);
   } catch (error) {
