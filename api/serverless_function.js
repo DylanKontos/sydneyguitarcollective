dc27f780c6b442614b285aea1b754c2944a77e02
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-05-14 12:41:38 +1000

    Update serverless_function.js

--- api/serverless_function.js
+++ api/serverless_function.js
@@ -1,10 +1,13 @@
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+
 async function refreshAccessToken() {
   // Use the Vercel environment variables for the client ID, client secret, and refresh token
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
   // Use the googleapis library to generate a new access token
-  const authClient = new google.auth.OAuth2(clientId, clientSecret);
+  const authClient = new OAuth2(clientId, clientSecret);
   const { access_token } = await authClient.getAccessToken();
   // Return the new access token
   return access_token;
