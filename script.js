b58ea80f9afa11759757c4f65d61c4965cf8df0d
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-05-16 20:34:51 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const submitButton = document.getElementById('submitBtn');
+  const popup = document.getElementById('popup');
+
+  submitButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    // Disable the submit button to prevent multiple submissions
+    submitButton.disabled = true;
+
+    // Show the loading message
+    showPopup('Processing...');
+
+    const firstname = encodeURIComponent(document.getElementById("firstname").value);
+    const lastname = encodeURIComponent(document.getElementById("lastname").value);
+    const email = encodeURIComponent(document.getElementById("email").value);
+    const url = `https://sydneyguitarcollective.vercel.app/api/serverless_function?firstname=${firstname}&lastname=${lastname}&email=${email}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    // Assume you have a variable 'success' indicating if the operation was successful
+
+    // Display the success or error message
+    const statusCode = response.status;
+
+    if (statusCode === 200) {
+      showPopup('Contact added successfully!');
+    } else {
+      showPopup('Failed to add contact. Please try again.');
+    }
+    // Re-enable the submit button after the operation is complete
+    submitButton.disabled = false;
+  });
+
+  function showPopup(message) {
+    popup.textContent = message;
+    popup.style.display = 'block';
+
+    // Hide the popup after a certain duration (e.g., 3 seconds)
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 3000);
+  }
+});
