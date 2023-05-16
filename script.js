1835beaeea52421257876d7a912d378d50f5c63f
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-05-16 20:42:41 +1000

    Update script.js

--- script.js
+++ script.js
@@ -24,16 +24,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const statusCode = response.status;
 
     if (statusCode === 200) {
-      showPopup('Contact added successfully!');
+      showPopup('Contact added successfully!', true);
     } else {
-      showPopup('Failed to add contact. Please try again.');
+      showPopup('Failed to add contact. Please try again later.', false);
     }
     // Re-enable the submit button after the operation is complete
     submitButton.disabled = false;
   });
 
-  function showPopup(message) {
+  function showPopup(message, isSuccess) {
+    const popup = document.getElementById('popup');
     popup.textContent = message;
+
+    // Apply styles based on success or failure
+    if (isSuccess) {
+      popup.style.backgroundColor = '#C8FACD'; // Light green background
+      popup.style.borderColor = '#258746'; // Dark green border
+      popup.style.color = '#258746'; // Dark green text
+    } else {
+      popup.style.backgroundColor = '#FFD9D9'; // Light red background
+      popup.style.borderColor = '#B00020'; // Dark red border
+      popup.style.color = '#B00020'; // Dark red text
+    }
+
+    // Show the popup at the bottom of the screen
+    popup.style.top = 'auto';
+    popup.style.bottom = '20px';
+
+    // Display the popup
     popup.style.display = 'block';
 
     // Hide the popup after a certain duration (e.g., 3 seconds)
