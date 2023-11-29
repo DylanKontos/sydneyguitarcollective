fa8cd3c5b86a3c824723aafcea7fd7aace206b63
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-11-29 19:26:07 +1100

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,21 +11,6 @@ const HomePage = () => {
                 alt="Sydney Guitar Collective"
             /> 
             </div>
-            <br/>
-            <h2 style={{ color: '#cce3f1' }}>New concert announced! 26th of November @ Mosman Art Gallery, Tickets available here: </h2>
-            <a
-            href="https://events.humanitix.com/sydney-guitar-collective"
-            style={{
-                textDecoration: 'none',
-                color: '#fff',
-                backgroundColor: '#007bff',
-                padding: '10px 20px',
-                borderRadius: '5px',
-                display: 'inline-block',
-            }}
-            >
-            Get Tickets on Humanitix
-            </a>
         </div>
     );
 };
