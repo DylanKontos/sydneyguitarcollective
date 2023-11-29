99bef657d658b18ec4398b3130bd9108e517a7cb
josh8898 <44086623+josh8898@users.noreply.github.com>
2023-11-29 19:25:39 +1100

    Update EventsPage.js

--- src/components/EventsPage.js
+++ src/components/EventsPage.js
@@ -5,7 +5,7 @@ const EventsPage = () => {
         <div className="Events">
             <div>
             <h1>Events</h1>
-            <h2 style={{ color: '#cce3f1' }}>Upcoming: </h2> 
+            <h2 style={{ color: '#cce3f1' }}>Past: </h2> 
             <img
                 src={require("../assets/mosmanpromo.JPG")}
                 style={{ width: '50%', height: 'auto', minWidth: '350px' }}
