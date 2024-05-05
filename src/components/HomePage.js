1849579fd0f046cade3f6a279f4bd61b9f971143
josh8898 <44086623+josh8898@users.noreply.github.com>
2024-05-05 14:59:49 +1000

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,10 +6,24 @@ const HomePage = () => {
             <div>
             <h2>The Premier Young Classical Guitar Ensemble based in Sydney, Australia</h2>
             <img
-                src={require("../assets/SydneyGuitarCollective-9.jpg")}
+                src={require("../assets/IntrusionsPromo.jpg")}
                 style={{ width: '35%', height: 'auto', minWidth: '300px' }}
                 alt="Sydney Guitar Collective"
             /> 
+            <h2 style={{ color: '#cce3f1' }}>New concert announced! 18th of May @ Mosman Art Gallery, Tickets available here: </h2>
+            <a
+            href="https://events.humanitix.com/sydney-guitar-collective___intrusions___"
+            style={{
+                textDecoration: 'none',
+                color: '#fff',
+                backgroundColor: '#007bff',
+                padding: '10px 20px',
+                borderRadius: '5px',
+                display: 'inline-block',
+            }}
+            >
+            Get Tickets on Humanitix
+            </a>        
             </div>
         </div>
     );
