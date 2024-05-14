a097d703252b6959a56bade515faca54c65150d0
josh8898 <44086623+josh8898@users.noreply.github.com>
2024-05-14 19:31:43 +1000

    Update PageHeader.js

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,27 +1,22 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../assets/logo.svg';
+import { Link, useLocation } from 'react-router-dom';
+import Logo from '../assets/Logo.png';
 import './PageHeader.css';
 
 const PageHeader = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen((prevState) => !prevState);
-  };
+  const location = useLocation(); // Get the current location
+  const [activePage, setActivePage] = useState(location.pathname); // Set initial active page
 
   return (
-    <header className={`PageHeader ${menuOpen ? 'open' : ''}`}>
-      <img src={Logo} className="Logo" alt="logo" />
-      <button className="Burger" onClick={toggleMenu}>
-        ☰
-      </button>
-      <nav className={`Nav ${menuOpen ? 'open' : ''}`}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/events">Events</Link>
-        <Link to="/videos">Videos</Link>
+    <header className={`PageHeader`}>
+      <nav className={`Nav`}>
+      <Link to="/" className={`NavButton ${activePage === "/" ? "active" : ""}`} onClick={() => setActivePage("/")}>Home</Link>
+        <Link to="/about" className={`NavButton ${activePage === "/about" ? "active" : ""}`} onClick={() => setActivePage("/about")}>About</Link>
+        <Link to="/events" className={`NavButton ${activePage === "/events" ? "active" : ""}`} onClick={() => setActivePage("/events")}>Events</Link>
+        <Link to="/videos" className={`NavButton ${activePage === "/videos" ? "active" : ""}`} onClick={() => setActivePage("/videos")}>Videos</Link>
       </nav>
+      <span className="Title">Sydney Guitar Collective</span>
+      <img src={Logo} className="Logo" alt="logo" />
     </header>
   );
 };
