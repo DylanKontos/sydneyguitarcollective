11a4aaf30a64fcfd91fb7b2a24b847377ec61130
josh8898 <44086623+josh8898@users.noreply.github.com>
2024-05-16 21:50:32 +1000

    Update PageContentWithPadding.js

--- src/components/PageContentWithPadding.js
+++ src/components/PageContentWithPadding.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const PageContentWithPadding = ({ children }) => {
+const PageContentWithPadding = ({ children, headerHeight }) => {
+  let paddingValue;
+
+  // Check if headerHeight contains 'vh' to determine the unit
+  if (headerHeight.includes('vh')) {
+    paddingValue = '15vh';
+  } else {
+    paddingValue = '120px';
+  }
+
   return (
-    <div style={{ paddingTop: '15vh' }}> {/* Adjust the padding-top value to match the height of your PageHeader */}
+    <div style={{ paddingTop: paddingValue }}>
       {children}
     </div>
   );
