007963d2b823b10cda91cf7925e5f4d88f51a9d4
josh8898 <44086623+josh8898@users.noreply.github.com>
2024-05-16 19:34:00 +1000

    Update AboutPage.js

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import AboutUs from './AboutUs';
 import HeadshotSection from './HeadshotSection';
+import PageContentWithPadding from './PageContentWithPadding';
 
 const AboutPage = () => {
     return (
-        <div className="About">
-            <h1>About</h1>
-            <div>
-            <img
-                src={require("../assets/SydneyGuitarCollective-11.jpg")}
-                style={{ width: '40%', height: 'auto', minWidth: '300px' }}
-                alt="Sydney Guitar Collective"
-            /> 
-            <br/>
-            <br/>
-            <br/>
-            <AboutUs />
-            <br/>
-            <h2>Members</h2>
-            <HeadshotSection />
+        <PageContentWithPadding>
+            <div className="About">
+                <h1>About</h1>
+                <div>
+                <img
+                    src={require("../assets/SydneyGuitarCollective-11.jpg")}
+                    style={{ width: '40%', height: 'auto', minWidth: '300px' }}
+                    alt="Sydney Guitar Collective"
+                /> 
+                <br/>
+                <br/>
+                <br/>
+                <AboutUs />
+                <br/>
+                <h2>Members</h2>
+                <HeadshotSection />
+                </div>
             </div>
-        </div>
+        </PageContentWithPadding>
     );
 };
 
