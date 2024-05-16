5395e56cd9d545d522550cf36f9c91e69d48ba87
josh8898 <44086623+josh8898@users.noreply.github.com>
2024-05-16 21:13:30 +1000

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,13 +12,11 @@ const HomePage = () => {
                     style={{ width: '35%', height: 'auto', minWidth: '300px' }}
                     alt="Sydney Guitar Collective"
                 /> 
-                <h2 style={{ color: '#cce3f1' }}>New concert announced! 18th of May @ Mosman Art Gallery, Tickets available here: </h2>
+                <h2>New concert announced! 18th of May @ Mosman Art Gallery, Tickets available here: </h2>
                 <a
                 href="https://events.humanitix.com/sydney-guitar-collective___intrusions___"
                 style={{
                     textDecoration: 'none',
-                    color: '#fff',
-                    backgroundColor: '#007bff',
                     padding: '10px 20px',
                     borderRadius: '5px',
                     display: 'inline-block',
